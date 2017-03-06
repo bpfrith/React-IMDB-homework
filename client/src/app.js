fbc6629db5575485664e9600912535b981b1ba06
Ben Frith <bpfrith@gmail.com>
2017-03-06 23:27:18 +0000

    app.js modified

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var Shows = require('./components/Shows');
+
+var content = (
+  <div>
+    <h1>Shows</h1>
+    <Shows></Shows>
+  </div>
+);
+
 window.onload = function(){
-  ReactDOM.render(
-    <h1> App Started </h1>,
-    document.getElementById('app')
-  );
+  ReactDOM.render( content, document.getElementById('app') );
 }
